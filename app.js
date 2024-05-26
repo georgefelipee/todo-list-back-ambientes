ca903c37f1db7b51435ccbef7dd5173919e2f4e5
George Felipe <georgefelipe938@gmail.com>
2024-05-26 11:12:24 -0300

    login e cadastro ok

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 import express from "express";
 import connectMongoAtlas from "./src/config/dbConnect.js";
 import userRoutes from "./src/routes/userRoutes.js";
-
+import cors from 'cors';
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
+app.use(cors());
 
 app.listen(port,() => {
     console.log('Servidor On' + port);
