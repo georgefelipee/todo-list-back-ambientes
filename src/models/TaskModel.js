8b0ecc0a353e89eeb6c7bf2fc3f001d64c820291
George Felipe <georgefelipe938@gmail.com>
2024-05-27 10:34:49 -0300

    definidno status

--- src/models/TaskModel.js
+++ src/models/TaskModel.js
@@ -6,6 +6,7 @@ const TaskSchema = new mongoose.Schema({
     title: {type: String, required: true},
     description: {type: String, required: true},
     deadline: {type: Date, required: true},
+    status: {type: String, required: true, default: "pendente"},
   
 });
 
